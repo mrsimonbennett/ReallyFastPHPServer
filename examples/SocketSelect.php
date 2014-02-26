20d27a562eef0ba432351098640b5b0f13dc5e50
Simon Bennett <simon@bennett.im>
2014-02-26 12:29:40 +0000

    Fix SocketSelect Example
    Signed-off-by: Simon Bennett <simon@bennett.im>

--- examples/SocketSelect.php
+++ examples/SocketSelect.php
@@ -24,8 +24,6 @@ $runcount = 0;
  * @param $args
  */
 $process = function($get,$post,$server,$args) {
-
-    var_dump($get,$post,$server,$args);
     $app = new MrSimonBennett\RestFrameWork\Bootstrap\Application();
 
     $app->httpFromManual([],[],[],[],['REQUEST_METHOD' => $server->method, 'REQUEST_URI' => $server->uri],[]);
@@ -57,7 +55,6 @@ while (true)
         $clients[] = $newsock = socket_accept($sock);
 
         socket_getpeername($newsock, $ip);
-        echo "New client connected: {$ip}\n";
 
         // remove the listening socket from the clients-with-data array
         $key = array_search($sock, $read);
@@ -68,38 +65,28 @@ while (true)
     foreach ($read as $read_sock) {
         // read until newline or 1024 bytes
         // socket_read while show errors when the client is disconnected, so silence the error messages
-        $data = @socket_read($read_sock, 1024, PHP_NORMAL_READ);
 
-        // check if the client is disconnected
-        if ($data === false) {
-            // remove client for $clients array
-            $key = array_search($read_sock, $clients);
-            unset($clients[$key]);
-            echo "client disconnected.\n";
-            // continue to the next client to read from, if any
-            continue;
-        }
 
-        // trim off the trailing/beginning white spaces
-        $data = trim($data);
 
-        // check if there is any data after trimming off the spaces
-        if (!empty($data)) {
 
-            // send this to all the clients in the $clients array (except the first one, which is a listening socket)
-            foreach ($clients as $send_sock) {
+        $response = new MrSimonBennett\HTTP\Responce();
+        $request = 	new MrSimonBennett\HTTP\Request($response,new MrSimonBennett\HTTP\Get(),new MrSimonBennett\HTTP\Post(),new MrSimonBennett\HTTP\Server());
 
-                // if its the listening sock or the client that we got the message from, go to the next one in the list
-                if ($send_sock == $sock || $send_sock == $read_sock)
-                    continue;
+        $request->read($read_sock);
+        $request->process($process);
 
-                // write the message to the client -- add a newline character to the end of the message
-                socket_write($send_sock, $data."\n");
 
-            } // end of broadcast foreach
 
-        }
+        $response->Go($read_sock);
+
+        //Really Important Line. This Closes the socket correctly
+        @socket_shutdown($read_sock, STREAM_SHUT_WR);
+
+        @socket_close($read_sock);
+        $key = array_search($read_sock, $clients);
+        unset($clients[$key]);
+
 
     } // end of reading foreach
-    usleep(0);
+
 }
